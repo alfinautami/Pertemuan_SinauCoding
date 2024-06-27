f643cb6abd19f57d298a6efac5eb32fcb1fdf092
finautami02 <finautami02@gmail.com>
2024-06-27 14:58:27 +0700

    tugas ke empat

---
+++ Pertemuan4_Js_SinauCoding/script.js
@@ -0,0 +1,17 @@
+var totalBus = 10;
+var busOperation = 7;
+var numberBus = 1;
+var lemburBus = 8;
+
+for(numberBus = 1; numberBus <= totalBus; numberBus++) {
+    if(numberBus < busOperation){
+        console.log('Bus transjakara ' + numberBus + ' beroprasi dengan baik.')
+            } 
+            else if(numberBus === lemburBus) {
+                console.log('Bus transjakarta ' + numberBus + ' sedang Lembur.')
+            }
+            else {
+                console.log('Bus transjakarta ' + numberBus + ' sedang tidak beroprasi.')
+            } 
+
+}
