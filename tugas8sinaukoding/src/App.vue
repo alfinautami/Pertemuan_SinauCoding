6c20ca870357b6a1861b4976a92a75ffeb88fbf9
finautami02 <finautami02@gmail.com>
2024-07-11 21:56:51 +0700

    tugas delapan

---
+++ tugas8sinaukoding/src/App.vue
@@ -0,0 +1,55 @@
+<script>
+import DataTable from './components/DataTable.vue';
+import axios from 'axios';
+
+
+export default {
+  name: 'App',
+  components: {
+    DataTable
+  },
+  data() {
+    return {
+      headers: [],
+      users: []
+    };
+  },
+  methods: {
+    handleShowData(id, nama, position, no_telp, email) {
+      alert(`User ID: ${id}\nNama: ${nama}\nPosition: ${position}\nNo Telp: ${no_telp}\nEmail: ${email}`);
+    },
+  },
+  created() {
+    axios.get('../utils/Header.json')
+      .then(response => {
+        this.headers = response.data;
+        console.log("sukses")
+      })
+      .catch(error => {
+        console.error("data tidak terkoneksi", error);
+      });
+
+    axios.get('../utils/User.json')
+      .then(response => {
+        this.users = response.data;
+        console.log("sukses")
+      })
+      .catch(error => {
+        console.error("data tidak terkoneksi", error);
+      });
+  }
+}
+</script>
+
+
+<template>
+  <div id="app">
+    <DataTable :headers="headers" :users="users" @show-data="handleShowData" />
+  </div>
+</template>
+
+
+
+<style>
+
+</style>
---
+++ tugas8sinaukoding/src/components/DataTable.vue
@@ -0,0 +1,44 @@
+<script>
+export default {
+  props: {
+    headers: Array,
+    users: Array
+  },
+  methods: {
+    tampilUser(id, nama, position, no_telp, email) {
+      this.$emit('show-data', id, nama, position, no_telp, email);
+    }
+  }
+}
+</script>
+
+<template>
+    <div>
+      <table border="1">
+        <thead>
+          <tr>
+            <th v-for="header in headers" :key="header">{{ header }}</th>
+          </tr>
+        </thead>
+        <tbody>
+        <tr v-for="(user, index) in users" :key="user.id">
+          <td>{{ index + 1 }}</td>
+          <td>{{ user.nama }}</td>
+          <td>{{ user.position }}</td>
+          <td>{{ user.no_telp }}</td>
+          <td>{{ user.email }}</td>
+          <td><button @click="tampilUser(user.id, user.nama, user.position, user.no_telp, user.email)">Show Data</button></td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
+  </template>
+  
+  
+  
+  <style>
+    th,td {
+        padding: 8px;
+    }
+  </style>
+  
